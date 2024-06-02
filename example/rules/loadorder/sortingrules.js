9d8e0f24f154f40991991d4d34ea7850f1e12de8
Bukowa <gitbukowa@gmail.com>
2024-06-03 00:03:21 +0200

    feat: start adding functions for load order `rules.json`
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

---
+++ example/rules/loadorder/sortingrules.js
@@ -0,0 +1,132 @@
+const _ = require("lodash");
+const ajv = require("ajv");
+const {inspect} = require("util");
+
+const RESERVED_MOD_KEYS = [
+    "*" // wildcard for all mods
+]
+
+class RulesDefinitionError extends Error {
+}
+
+/**
+ * Get dependencies that are not defined in the rules.
+ * @param rules
+ * @param modKey
+ * @returns {string[]} - array of modKeys that are not defined in the rules.
+ * */
+function getNotDefinedDependencies(rules, modKey) {
+    return _.difference(
+        _.keys(rules['database'][modKey]['dependencies']),
+        _.keys(rules['database'])
+    );
+}
+
+/**
+ * Get loadBefore dependencies that are not defined in the rules.
+ * @param rules
+ * @param modKey
+ * @returns {string[]}  - array of modKeys that are not defined in the rules.
+ */
+function getNotDefinedLoadBefore(rules, modKey) {
+    return _.difference(
+        _.keys(rules['database'][modKey]['loadBefore']).filter(
+            key => !RESERVED_MOD_KEYS.includes(key)
+        ),
+        _.keys(rules['database'])
+    )
+}
+
+/**
+ * Get loadAfter dependencies that are not defined in the rules.
+ * @param rules
+ * @param modKey
+ * @returns {string[]}  - array of modKeys that are not defined in the rules.
+ */
+function getNotDefinedLoadAfter(rules, modKey) {
+    return _.difference(
+        _.keys(rules['database'][modKey]['loadAfter']).filter(
+            key => !RESERVED_MOD_KEYS.includes(key)
+        ),
+        _.keys(rules['database'])
+    )
+}
+
+const VALIDATE_HAS_NO_RESERVED_KEYS_ERRORS = {
+    'database': new RulesDefinitionError(
+        `'database' key contains any of reserved keys: ${RESERVED_MOD_KEYS}`,
+    ),
+    'dependencies': new RulesDefinitionError(
+        `'dependencies' key contains any of reserved keys: ${RESERVED_MOD_KEYS}`,
+    ),
+}
+
+/**
+ * Validate that the rules do not contain reserved keys.
+ * @param rules
+ */
+function validateHasNoReservedKeys(rules) {
+    if (_.some(_.keys(rules['database']), key =>
+        _.includes(RESERVED_MOD_KEYS, key))) {
+        throw VALIDATE_HAS_NO_RESERVED_KEYS_ERRORS['database'];
+    }
+
+    if (_.keys(rules['database']).some(key =>
+        _.some(_.keys(rules['database'][key]['dependencies']), key =>
+            _.includes(RESERVED_MOD_KEYS, key)))) {
+        throw VALIDATE_HAS_NO_RESERVED_KEYS_ERRORS['dependencies'];
+    }
+}
+
+/**
+ * Validate the rules object against a schema.
+ * @param rules
+ */
+function validateRules(rules) {
+    const schema = {
+        type: "object",
+        required: ["database"],
+        additionalProperties: false,
+        properties: {
+            "database": {
+                type: "object",
+                additionalProperties: {
+                    required: ["name"],
+                    type: "object",
+                    properties: {
+                        "name": {type: "string"},
+                        "loadBefore": {
+                            type: "object",
+                            additionalProperties: {
+                                type: "object",
+                                properties: {
+                                    "stable": {type: "string"},
+                                },
+                            }
+                        },
+                        "loadAfter": {
+                            type: "object",
+                            additionalProperties: {
+                                type: "object",
+                            }
+                        },
+                    },
+                }
+            }
+        },
+    }
+    const validate = new ajv().compile(schema);
+    const valid = validate(rules);
+    if (!valid) {
+        throw new RulesDefinitionError(inspect(validate.errors));
+    }
+}
+
+module.exports = {
+    getNotDefinedDependencies,
+    getNotDefinedLoadBefore,
+    getNotDefinedLoadAfter,
+    validateHasNoReservedKeys,
+    VALIDATE_HAS_NO_RESERVED_KEYS_ERRORS,
+    validateRules,
+}
---
+++ test/loadorder/sortingrules.test.js
@@ -0,0 +1,180 @@
+const assert = require("assert");
+const _unit = require("../../example/rules/loadorder/sortingrules");
+
+
+function createRules(rules) {
+    _unit.validateRules(rules);
+    return rules;
+}
+
+describe('getNotDefinedDependencies', () => {
+    it("should return an array of dependencies that are not defined", () => {
+        let rules = createRules(
+            {
+                "database": {
+                    "mod1": {
+                        "name": "",
+                        "dependencies": {
+                            "mod2": {},
+                            "mod3": {}
+                        }
+                    },
+                    "mod2": {
+                        "name": "",
+                        "dependencies": {
+                            "mod1": {},
+                            "mod2": {}
+                        }
+                    },
+                    "mod3": {
+                        "name": "",
+                        "dependencies": {
+                            "mod1": {},
+                            "mod4": {},
+                            "mod5": {}
+                        }
+                    }
+                }
+            }
+        )
+        assert.deepEqual(_unit.getNotDefinedDependencies(rules, "mod1"), []);
+        assert.deepEqual(_unit.getNotDefinedDependencies(rules, "mod2"), []);
+        assert.deepEqual(_unit.getNotDefinedDependencies(rules, "mod3"), ["mod4", "mod5"]);
+    });
+});
+
+describe('getNotDefinedLoadBefore', () => {
+    it("should return an array of loadBefore dependencies that are not defined", () => {
+        let rules = createRules({
+                "database": {
+                    "mod1": {
+                        "name": "",
+                        "loadBefore": {
+                            "mod2": {},
+                            "mod3": {}
+                        }
+                    },
+                    "mod2": {
+                        "name": "",
+                        "loadBefore": {
+                            "mod1": {},
+                            "mod2": {}
+                        }
+                    },
+                    "mod3": {
+                        "name": "",
+                        "loadBefore": {
+                            "mod1": {},
+                            "mod4": {},
+                            "mod5": {},
+                        }
+                    }
+                }
+            }
+        )
+        assert.deepEqual(_unit.getNotDefinedLoadBefore(rules, "mod1"), []);
+        assert.deepEqual(_unit.getNotDefinedLoadBefore(rules, "mod2"), []);
+        assert.deepEqual(_unit.getNotDefinedLoadBefore(rules, "mod3"), ["mod4", "mod5"]);
+    });
+});
+
+describe('getNotDefinedLoadAfter', () => {
+    it("should return an array of loadAfter dependencies that are not defined", () => {
+        let rules = createRules({
+            "database": {
+                "mod1": {
+                    "name": "",
+                    "loadAfter": {
+                        "mod2": {},
+                        "mod3": {}
+                    }
+                },
+                "mod2": {
+                    "name": "",
+                    "loadAfter": {
+                        "mod1": {},
+                        "mod2": {}
+                    }
+                },
+                "mod3": {
+                    "name": "",
+                    "loadAfter": {
+                        "mod1": {},
+                        "mod4": {},
+                        "mod5": {},
+                    }
+                }
+            }
+        })
+        assert.deepEqual(_unit.getNotDefinedLoadAfter(rules, "mod1"), []);
+        assert.deepEqual(_unit.getNotDefinedLoadAfter(rules, "mod2"), []);
+        assert.deepEqual(_unit.getNotDefinedLoadAfter(rules, "mod3"), ["mod4", "mod5"]);
+    });
+});
+
+describe('validateHasNoReservedKeys', () => {
+
+    it("should not throw an error if no reserved keys are found", () => {
+            let rules = createRules({
+                "database": {
+                    "mod1": {
+                        "name": "",
+                        "loadBefore": {
+                            "mod2": {},
+                            "mod3": {}
+                        }
+                    },
+                    "mod2": {
+                        "name": "",
+                        "loadBefore": {
+                            "mod1": {},
+                            "mod2": {}
+                        }
+                    },
+                    "mod3": {
+                        "name": "",
+                        "loadBefore": {
+                            "mod1": {},
+                            "mod4": {},
+                            "mod5": {},
+                        }
+                    }
+                }
+            })
+            assert.doesNotThrow(() => _unit.validateHasNoReservedKeys(rules));
+        }
+    )
+
+    it("should throw an error if reserved keys are found in database", () => {
+        let rules = createRules({
+            "database": {
+                "*": {
+                    "name": "",
+                    "loadBefore": {
+                        "mod1": {},
+                        "mod2": {}
+                    }
+                },
+            }
+        })
+        assert.throws(() => _unit.validateHasNoReservedKeys(rules),
+            _unit.VALIDATE_HAS_NO_RESERVED_KEYS_ERRORS['database']);
+    });
+
+    it("should throw an error if reserved keys are found in dependencies", () => {
+        let rules = createRules({
+            "database": {
+                "mod1": {
+                    "name": "",
+                    "dependencies": {
+                        "*": {},
+                        "mod2": {}
+                    }
+                },
+            }
+        })
+        assert.throws(() => _unit.validateHasNoReservedKeys(rules),
+            _unit.VALIDATE_HAS_NO_RESERVED_KEYS_ERRORS['dependencies']);
+    });
+
+})
