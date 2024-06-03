e923454dcfc7fcd077e32dd503a7ea90e6aac143
Bukowa <gitbukowa@gmail.com>
2024-06-04 01:47:44 +0200

    chore: add initial ui app with tauri vue bun
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

--- build-exe.sh
+++ build-exe.sh
@@ -8,3 +8,4 @@ node -e "require('fs').copyFileSync(process.execPath, './dist/main.exe')"
 signtool remove /s ./dist/main.exe
 npx postject ./dist/main.exe NODE_SEA_BLOB ./dist/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 signtool sign /fd SHA256 ./dist/main.exe
+echo "Size of the binary: $(du -h ./dist/main.exe | cut -f1)"
