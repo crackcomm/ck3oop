74676ebce4a1ac4330ab62a5e522847cd2d96b62
Bukowa <gitbukowa@gmail.com>
2024-06-04 02:01:11 +0200

    fix(ui): hide initial blank window https://github.com/tauri-apps/tauri/issues/5170
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

--- ck3oop-ui/src/main.ts
+++ ck3oop-ui/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import {appWindow} from "@tauri-apps/api/window";
 import {helloWorld} from "ck3oop-core-js";
 
 helloWorld("ck3oop-ui")
 createApp(App).mount("#app");
+appWindow.show().then(() => {});
